1517b43065da787a9bd9bf028bf8649d4cd545f1
mibu119 <horang296@gmail.com>
2023-05-22 17:49:05 +0900

    🚸 💄 intersectionobserver 사용하고 UI 변경

--- frontend/src/SearchResult.js
+++ frontend/src/SearchResult.js
@@ -12,6 +12,7 @@ class SearchResult {
 
     this.data = initialData;
     this.onClick = onClick;
+    this.onNextPage = onNextPage;
 
     this.render();
   }
@@ -21,12 +22,30 @@ class SearchResult {
     this.render();
   }
 
+  listObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        // 이미지를 로드한다. lazyLoad
+        item.target.querySelector("img").src =
+          item.target.querySelector("img").dataset.src;
+        // 아이템이 화면에 보일 때
+        this.data.length;
+        let dataIndex = Number(item.target.dataset.index);
+        // 마지막 요소를 찾아낸다
+        // 마지막 요소라면? nextPage 호출
+        if (dataIndex === this.data.length) {
+          this.onNextPage();
+        }
+      }
+    });
+  });
+
   render() {
     this.$searchResult.innerHTML = this.data
       .map(
         (cat) => `
-          <li class="item">
-            <img src=${cat.url} alt=${cat.name} />
+          <li class="item" data-index=${index}>
+            <img src="https://via.placeholder.com/200x300" data-src=${cat.url} alt=${cat.name} />
           </li>
         `
       )
@@ -36,6 +55,8 @@ class SearchResult {
       $item.addEventListener("click", () => {
         this.onClick(this.data[index]);
       });
+
+      this.listObserver.observe($item);
     });
   }
 }
